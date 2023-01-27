22d8c5342e1f8765b8085f5e4dafa411451822ea
azmialwerfalli <drama.com.aa@gmail.com>
2023-01-27 22:51:55 +0200

    modified:   src/App.vue 	new file:   src/components/Block.vue 	deleted:    src/components/HelloWorld.vue 	new file:   src/components/Results.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Azmi Reaction Timer</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -20,7 +19,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: crimson;
   margin-top: 60px;
 }
 </style>
