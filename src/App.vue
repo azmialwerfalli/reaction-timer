61c334aaa1d87cd48d86f845894f64c79b5b7978
azmialwerfalli <drama.com.aa@gmail.com>
2023-01-28 21:27:49 +0200

    Game Setup (Block Showing)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>
+  <!-- <div> -->
     <h1>Azmi Reaction Timer</h1>
-  </div>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
+  <!-- </div> -->
 </template>
 
 <script>
-
+import Block from './components/Block.vue'
 export default {
   name: 'App',
   components: {
+    Block,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start(){
+      this.delay =  2000 + Math.random() * 5000
+      this.isPlaying = true
+      // console.log(Math.floor(this.delay))
+    }
   }
 }
 </script>
