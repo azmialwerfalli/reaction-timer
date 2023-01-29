b43b4c47778873fac956f2569145a3d12f431e88
azmialwerfalli <drama.com.aa@gmail.com>
2023-01-29 20:48:02 +0200

    results

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,45 @@
 <template>
   <!-- <div> -->
-    <h1>Azmi Reaction Timer</h1>
+  <h1>Azmi Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay"/>
+  <Results v-if="showResults" :score="score" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <!-- <p v-if="showResults">{{ score }}</p> -->
   <!-- </div> -->
 </template>
 
 <script>
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
 
-import Block from './components/Block.vue'
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Block,
+    Results,
   },
   data() {
     return {
       isPlaying: false,
-      delay: null
-    }
+      delay: null,
+      score: null,
+      showResults: false,
+    };
   },
   methods: {
-    start(){
-      this.delay =  2000 + Math.random() * 5000
-      this.isPlaying = true
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
       // console.log(Math.floor(this.delay))
-    }
-  }
-}
+    },
+    endGame(reactionTime) {
+      // this.$emit('score', reactionTime)
+      (this.score = reactionTime), (this.isPlaying = false);
+      this.showResults = true;
+    },
+  },
+};
 </script>
 
 <style>
