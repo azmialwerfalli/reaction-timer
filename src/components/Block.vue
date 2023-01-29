5092921cfddb95e1e89db2073726d88601c3d80f
azmialwerfalli <drama.com.aa@gmail.com>
2023-01-29 19:22:48 +0200

    modified:   src/components/Block.vue

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="block" v-if="showBlock">
+  <div class="block" v-if="showBlock" @click="stopTimer">
     click me
+  </div>
+  <div class="result">
+
   </div>
 </template>
 
@@ -9,16 +12,29 @@ export default {
     props: ['delay'],
     data() {
         return {
-            showBlock: false
+            showBlock: false,
+            timer: null,
+            reactionTime: 0,
         }
     },
     mounted() {
         console.log("component mounted")
         setTimeout(()=> {
             this.showBlock = true
-            console.log(this.delay)
+            // console.log(this.delay)
+            this.startTimer()
         }, this.delay)
     },
+    methods: {
+        startTimer(){
+            this.timer = setInterval(() => {this.reactionTime+=10},10)
+        },
+        stopTimer(){
+            console.log(this.reactionTime)
+            clearInterval(this.timer)
+            
+        }
+    }
 
 }
 </script>
